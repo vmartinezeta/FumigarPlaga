3066f6264ed0dc45f827186c3d66d6c56ec9a3d0
Víctor Martínez <vito.marodriguez@gmail.com>
2025-07-26 14:43:04 -0600

    Notificaciones de potenciadores

--- src/game/sprites/PotenciadorGroup.js
+++ src/game/sprites/PotenciadorGroup.js
@@ -8,73 +8,80 @@ export default class PotenciadorGroup extends Phaser.GameObjects.Group {
         this.scene = scene;
         this.config = config;
         this.scene.physics.add.existing(this, true);
-        this.recientes = 0;
         this.id = 0
         this.alertas = [];
         this.primerasVecesAlerta = {
-            tanque:0,
-            tanqueMax: 3,
-            vida: 0,
-            vidaMax: 3
+            tanque: 3,
+            vida: 3,
         };
     }
 
     addPotenciador(potenciador) {
-        if (potenciador instanceof TanqueConAgua && this.primerasVecesAlerta.tanque < this.primerasVecesAlerta.tanqueMax) {
-            this.createAlerta(100, 60, "Use la tecla A para suministrar el agua")
-            this.primerasVecesAlerta.tanque ++
-            this.actualizar()
+        if (potenciador instanceof TanqueConAgua && this.primerasVecesAlerta.tanque > 0) {
+            this.createAlerta(this.scene.game.config.width / 2, 60, "Use la tecla A para suministrar el agua")
+            this.primerasVecesAlerta.tanque--
+            this.reordenar()
         }
 
-        if (potenciador instanceof Vida && this.primerasVecesAlerta.vida < 1) {
-            this.createAlerta(100, 60, "Use la tecla A para recoger la vida")
-            this.primerasVecesAlerta.vida ++
-            this.actualizar()
+        if (potenciador instanceof Vida && this.primerasVecesAlerta.vida > 0) {
+            this.createAlerta(this.scene.game.config.width / 2, 60, "Use la tecla A para recoger la vida")
+            this.primerasVecesAlerta.vida--
+            this.reordenar()
         }
 
         this.add(potenciador)
     }
 
     createAlerta(x, y, texto) {
-        this.id ++
-        const duplicado = this.alertas.find(alerta => alerta.text ===texto)
-        if (duplicado) {
-            this.scene.time.removeEvent(duplicado.evento)
-            this.onEliminar(duplicado)
+        const encontrado = this.alertas.find(alerta => alerta.text === texto)
+        if (encontrado) {
+            this.scene.time.removeEvent(encontrado.onEliminar)
+            this.onEliminar(encontrado)
         }
+
+        const forma = this.scene.add.graphics();
+        forma.fillStyle(0x000000, 1);
+        forma.fillRect(x-300, y-84, 600, 50);
+        this.add(forma)
 
         const notificacion = this.scene.add.text(
             x, y, texto, {
             fontFamily: 'Arial Black', fontSize: 20, color: '#ffffff',
             stroke: '#000000', strokeThickness: 10,
             align: 'center'
-        }).setOrigin(0).setDepth(100)
-        notificacion.id = this.id
-        this.add(notificacion)
-        const evento = this.scene.time.delayedCall(4000, this.onEliminar, [notificacion], this);
-        notificacion.evento = evento
-        this.alertas.push(notificacion)
+        }).setOrigin(0.5).setDepth(100)
+        notificacion.id = ++this.id;
+        notificacion.forma = forma
+        this.add(notificacion);
+        notificacion.onEliminar = this.scene.time.delayedCall(4000, this.onEliminar, [notificacion], this);
+        this.alertas.push(notificacion);
 
         this.scene.tweens.add({
             targets: notificacion,
             alpha: 0.5,
             duration: 500,
             yoyo: true,
-            repeat: -1          
+            repeat: -1
         });
     }
 
     onEliminar(notificacion) {
+        this.remove(notificacion.forma, true, true)
         this.remove(notificacion, true, true)
         const idx = this.alertas.findIndex(alerta => alerta.id === notificacion.id)
         this.alertas.splice(idx, 1)
+        this.reordenar()
     }
 
-    actualizar() {
+    reordenar() {
         let anterior = null
-        for(const alerta of this.alertas) {
+        for (const alerta of this.alertas) {
             if (anterior) {
                 alerta.y = anterior.y + 60
+                alerta.forma.y = alerta.y
+            } else {
+                alerta.y = 60
+                alerta.forma.y = alerta.y
             }
             anterior = alerta
         }
