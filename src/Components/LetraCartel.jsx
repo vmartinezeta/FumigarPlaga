04b8d4d8efd4c2f5d1c2052588b7e3759d458070
Víctor Martínez <vito.marodriguez@gmail.com>
2025-07-31 13:39:04 -0600

    Preliminares de fumigar plagas

--- src/Components/LetraCartel.jsx
+++ src/Components/LetraCartel.jsx
@@ -2,7 +2,7 @@
 export default function LetraCartel({letra}) {
     return <div className="rotulo-ppal__letra" style={{
         left:letra.origen.x-25+45,
-        top:letra.origen.y+20
+        top:letra.origen.y+40
     }}>
         <h1 className="rotulo-ppal__simbolo">{letra.caracter}</h1>
     </div>
